8479b44136ddfd9112beba2391c7c023b6407f5b
impartial-router-9741 <elena.tjahjono@yahoo.com>
2021-01-22 09:03:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz (int)
-  if int divisible by 3 == 0
-
-    expect (fizz_3).to eq("Fizz")
+  if int % 3 == 0
+    "Fizz"
   end
 end
